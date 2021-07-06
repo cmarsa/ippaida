e853d55f1fb21368bc12b387af12e059961c8c62
mar <mar.dls@tutamail.com>
2021-07-06 00:30:05 -0500

    integer operations and usethree fix.

--- chapter01/excercises/usethree.py
+++ chapter01/excercises/usethree.py
@@ -1,3 +1,11 @@
+# usethree.py
+"""
+Modify
+useargument.py to compose a program usethree.py that takes
+three names and writes a proper sentence with the names in the reverse of the or-
+der they are given, so that, for example, python usethree.py Alice Bob Carol
+writes the string 'Hi Carol, Bob, and Alice' .
+"""
 import sys
 import stdio
 
