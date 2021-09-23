61665054330770fd0d1f8ac4dc404ef51a7e4f9f
positronn <marcodelossantos@protonmail.com>
2021-09-23 18:23:12 -0500

    1.3.31 Compose a program that writes the coordinates of a random point (a, b, c) on the surface of a sphere. To generate such a point, use Marsaglia’s method: Start by picking a random point (x, y) in the unit disk using the method described at the end of this section. Then, set a to 2 ...

---
+++ chapter01/excercises/marsagliasmethod.py
@@ -0,0 +1,22 @@
+# marsagliasmethod.py
+
+# Compose a program that writes the coordinates of a random
+# point (a, b, c) on the surface of a sphere.
+# To generate such a point, use Marsaglias method.
+
+import stdio
+import random
+import math
+
+# get random point from circle
+while True:
+    x = 1.0 + 2.0 * random.uniform(-1, 1)
+    y = 1.0 + 2.0 * random.uniform(-1, 1)
+    if x*x + y*y <= 1.0:
+        break
+
+a = 2 * x * math.sqrt(1 - x**2 - y**2)
+b = 2 * math.sqrt(1 - x**2 - y**2)
+c = 1 - 2 * (x**2 + y**2)
+
+stdio.writeln('(' + str(a) + ', ' + str(b) + ', ' + str(c) + ')')
