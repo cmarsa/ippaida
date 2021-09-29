380bcba75d4dbbd171456c02df6b45ff6effba21
positronn <marcodelossantos@protonmail.com>
2021-09-29 14:09:37 -0500

    Dealing n 5 card poker hands from shuffled deck.

---
+++ chapter01/arrays/deal.py
@@ -0,0 +1,42 @@
+# deal.py
+
+# 1.4.8 Compose a program deal.py that takes a command-line argument n and
+# writes n poker hands (five cards each) from a shuffled deck, separated
+# by blank lines.
+
+import random
+import sys
+import stdarray
+import stdio
+
+n = int(sys.argv[1])
+if n > 10:
+    raise ValueError, 'n must be 10 or less.'
+
+SUITS = ['clubs', 'diamonds', 'hearts', 'spades']
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+         'J', 'Q', 'K', 'A']
+
+# create deck
+deck = []
+for rank in RANKS:
+    for suit in SUITS:
+        card = rank + '-' + suit
+        deck += [card]
+
+# shuffle the deck several times
+deck_length = len(deck)
+for j in range(0, 5):
+    for i in range(0, deck_length):
+        r = random.randrange(0, n)
+        temp = deck[r]
+        deck[r] = deck[i]
+        deck[i] = temp
+
+# draw cards
+draw_index = 0
+for i in range(0, n):
+    for j in range(0, 5):
+        stdio.write(deck[draw_index] + str(', '))
+        draw_index += 1
+    stdio.writeln()
