47496c1f999d331e2c9cbeaf9d95baf2d28f7550
positronn <marcodelossantos@protonmail.com>
2021-09-29 23:12:01 -0500

    re-initializing array every new shuffle.

--- chapter01/arrays/shuffletest.py
+++ chapter01/arrays/shuffletest.py
@@ -13,8 +13,8 @@ import random
 import stdio
 import stdarray
 
-n = int(sys.argv[1])
-m = int(sys.argv[2])
+m = int(sys.argv[1])
+n = int(sys.argv[2])
 
 # initialize array a = [0, 1, ..., n - 1]
 a = stdarray.create1D(m, 0)
@@ -24,6 +24,8 @@ for i in range(0, m):
 table = stdarray.create2D(m, m, 0)
 # shuffle the deck several times
 for j in range(0, n):
+    for i in range(0, m):
+        a[i] = i
     for i in range(0, len(a)):
         r = random.randrange(0, m)
         temp = a[r]
@@ -32,20 +34,24 @@ for j in range(0, n):
         table[i][r] += 1
 
 # print header of contingency table
-stdio.write('   ')
+stdio.write('    ')
 for i in range(0, m):
     if len(str(i)) < 2:
         stdio.write('  ')
+    else:
+        stdio.write(' ')
     stdio.write(str(i) + ' ')
 stdio.writeln()
 
 # print frequencies on m x m basis
 i = 0
 for row in table:
-    stdio.write(str(i) + '  ')
+    if len(str(i)) < 2:
+        stdio.write(' ')
+    stdio.write(str(i) + ' |')
     for element in row:
         if len(str(element)) < 3:
-            stdio.write(' ')
+            stdio.write(' '*(3-len(str(element))))
         stdio.write(str(element) + ' ')
     stdio.writeln()
     i += 1
