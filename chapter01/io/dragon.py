1127e6b97bc1d045a9ed1b33621ca917c99a1a8e
positronn <marcodelossantos@protonmail.com>
2021-10-06 18:42:09 -0500

    `dragon.py`

---
+++ chapter01/io/dragon.py
@@ -0,0 +1,11 @@
+# dragon.py
+
+# python dragon.py < in.txt | python dragon.py | python dragon.py
+
+import stdio
+dragon = stdio.readString()
+nogard = stdio.readString()
+stdio.write(dragon + 'L' + nogard)
+stdio.write(' ')
+stdio.write(dragon + 'R' + nogard)
+stdio.writeln()
